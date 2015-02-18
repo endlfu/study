46716193420f66ae009fb74c8ed8ed96b4ef1591
Masaaki Endo <masaaki.endo@g-pc-5441.intra.gree-office.net>
2015-02-18 19:55:47 +0900

    unity tutorial

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float speed = 10      ;
+
+	// ゲームオブジェクト生成から削除するまでの時間
+	public float lifeTime = 1   ;
+
+  	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = transform.up.normalized * speed;
+
+		// lifeTime秒後に削除
+		Destroy (gameObject, lifeTime);
+	 }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
